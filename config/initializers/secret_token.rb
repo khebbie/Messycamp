f51f0dfafe9f202a6de688346431cb016e2bcbaf
Klaus Hebsgaard <klaus@hebsgaard.dk>
2012-01-06 23:21:13 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messycamp::Application.config.secret_token = 'e6d8ce584c40e2ac5224a50b52f1fde519d9ce9349fdf9f2fa79378c11e6a532a14c5bebf245aff081c87af32e3e3f2308b93c3a2ce3b0d5d9d01059755d7f9e'
